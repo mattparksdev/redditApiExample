d47fd3dc1ea84c0c69e2fd863a1a267ad09ba600
Matthew Parks <mattparks.dev@gmail.com>
2015-07-30 15:22:11 -0400

    takes json param not jquery; my bad

--- AjaxcalltoReddit/AjaxexampleforReddit/script.js
+++ AjaxcalltoReddit/AjaxexampleforReddit/script.js
@@ -3,18 +3,26 @@ $('.btn').click(function() {
   
     $('.text').text('loading bitches....');
   // On click on the button, make an AJAX request to reddit for results of cute puppies
-  $.getJSON({
+  $.ajax({
     type:"GET",
+    data: {
+      q: "puppy",
+      restrict_sr: "true"
+    },
 
-    url:"    http://www.reddit.com/r/askreddit/search.json?q=puppy&restrict_sr=true",
+    url:"https://www.reddit.com/r/aww/search.json",
     success: function(response) {
       $('.text').html('');
-  for (var i=0; i < response.data.children.length; i++ ) {
-      var image = "<img src='" +
-      response.data.children[i].data.thumbnail + "/>";
-    $('.text').append(image);
-  };
+      var children = response.data.children;
+  for (var i=0; i < children.length; i++ ) {
+      
+    if (children[i].data.thumbnail !== "self" && children[i].data.thumbnail !== "default") {
+          $('.text').append('<img src="' + children[i].data.thumbnail + '" />');
+  }
     
     }
   });
 });
+
+
+
